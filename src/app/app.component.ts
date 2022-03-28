ed83eb5f267ed3275128c4b54575d9a2c21fae80
Syderia <Stabber7997@gmail.com>
2022-03-28 14:21:30 +0800

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,40 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tailWindSample';
   isCategoryMenuShow="hidden" ;
-  category=["Woman", "Man","Kid","accessory" ]
+  category=["Woman", "Man","Kid","Accessory" ]
+
+  clothesData=[
+    {
+      name:1,
+      size:["S","M","L"]
+    },
+    {
+      name:2,
+      size:["S","M","L"]
+    },
+    {
+      name:3,
+      size:["XS","S","M","L"]
+    },
+    {
+      name:4,
+      size:["S","M","L"]
+    }, 
+    {
+      name:5,
+      size:["S","M","L"]
+    }, 
+    {
+      name:6,
+      size:["S","M","L", "X", "XL"]
+    },
+    {
+      name:7,
+      size:["S","M","L"]
+    }
+    
+  ]
+
 
   categoryMenuClick(){
     if( this.isCategoryMenuShow =="block")
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ CardModule } from './card/card.module'
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
