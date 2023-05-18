ec21edd671472a045700accb0bdbcbbbac6e562e
Ashley <Ashley_li@wistrom.com>
2023-05-18 10:37:08 +0800

    update lh ci

--- lighthouse.js
+++ lighthouse.js
@@ -1,13 +1,3 @@
-// module.exports = {
-//     ci: {
-//       collect: {
-//         url: ['http://localhost:4200'],
-//         staticDistDir: './dist/tailWindSample',
-//       },
-//       // 其他設定...
-//     },
-//   };
-
   module.exports = {
     ci: {
       collect: {
